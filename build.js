0cd4f8d7e54a8528fa5dd87126cc635515d9555d
Alex <atenefyr@gmail.com>
2018-02-20 18:15:10 -0600

    add OS change capability to build.js

--- build.js
+++ build.js
@@ -2,6 +2,7 @@ const {compile} = require('nexe');
 
 compile({
     input: './main.js',
-    output: './tosabbreviator_XYZ.exe',
-    fakeArgv: 'production'
+    output: './tosabbreviator_XYZ',
+    fakeArgv: 'production',
+    target: (process.argv[2] || process)
 })
