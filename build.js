c8267fe0083c4da3dc1bb0d1a10d8fb72f78343f
Alex <atenefyr@gmail.com>
2018-02-20 18:37:46 -0600

    improve build.js

--- build.js
+++ build.js
@@ -1,8 +1,28 @@
 const {compile} = require('nexe');
 
+let chosenTarget = process.argv[2];
+let outputLocation = './tosabbreviator_XYZ';
+
+switch(Number(chosenTarget)) {
+    case 0:
+        chosenTarget = 'windows-x64'
+        break;
+    case 1:
+        chosenTarget = 'mac-x64'
+        outputLocation = './tosabbreviator_XYZ_mac';
+        break;
+    case 2: // town of salem steam for linux doesn't exist but maybe in the future?
+        chosenTarget = 'linux-x64'
+        outputLocation = './tosabbreviator_XYZ_linux';
+        break;
+}
+
+console.log('Compiling for ' + chosenTarget);
+
 compile({
     input: './main.js',
-    output: './tosabbreviator_XYZ',
+    output: outputLocation,
     fakeArgv: 'production',
-    target: (process.argv[2] || process)
-})
+    loglevel: (process.argv[3] || 'info'),
+    target: (chosenTarget || process)
+});
